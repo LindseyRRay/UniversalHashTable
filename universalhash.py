64940f4ad1dd04ca08f70362bb7828d900c5966b
lrraymond13 <lrraymond13@gmail.com>
2014-12-05 22:46:11 -0500

    "getting, setting and deleting items functioning"

--- universalhash.py
+++ universalhash.py
@@ -4,13 +4,13 @@
 #let a is a random number from 0 to 1, and b is any number between o and p-1
 #hashf(a,b))(k) = ((ak+b)%p)%m) where m is length of hash table
 
-import sys, random
+import sys, random, pdb
 
 class UniversalHash:
 
 	def __init__(self, size):
 		self.size = int(size)
-		self.hashtable = (None,)*self.size
+		self.hashtable = tuple([[None, None] for x in range(self.size)])
 		self.p = self._get_prime()
 		self.a = random.random()
 		self.b = random.randint(0, self.p-1)
@@ -20,44 +20,60 @@ class UniversalHash:
 			return False
 		return not any((n%i==0 for i in range(3,n-1)))
 
-	def _get_prime(self):
-		p = random.randint(1000000,10000000)
+	def _get_prime(self, p=0):
+		if p ==0:
+			p = random.randint(1000000,10000000)
 		while not self._isprime(p):
 			p+=1
 		return p
 
 	def hash_func(self, hashkey):
-		print("Hash Function parameters: P is %s, A is %s, B is %s"%(self.p, self.a, self.b))
-		return ((self.a*hashkey + self.b)%self.p)%self.size
+		return ((round(self.a*hashkey + self.b))%self.p)%self.size
 
 	def probing_hash(self, hashkey):
-		print("Probing")
 		probe_hash = 1 + hashkey%(self.size -1)
 		while not self._isprime(probe_hash):
 			probe_hash+=1
 		return probe_hash%self.size
 
-	TROUBLESHOOTING HASH TABLE!
-	SOME RECURSION ERRORS
+	def __getitem__(self, key):
+		i=0
+		hashval = (self.hash_func(key) + i*self.probing_hash(key))%self.size 
+		if self.hashtable[hashval][0] == None:
+			raise KeyError("Key does not exist")
+		while self.hashtable[hashval][0] != key and i < self.size:
+			i+=1
+			hashval = (self.hash_func(key) + i*self.probing_hash(key))%self.size 
+		if i < self.size:
+			return self.hashtable[hashval][1]
+		else:
+			raise RuntimeError("Unable to find item")
 
-	# def __getattr__(self, key):
-	# 	i=0
-	# 	hashval = self.hash_func(key) + i*self.probing_hash(key)
-	# 	if self.hashtable[hashval][0] == None:
-	# 		raise KeyError("Key does not exist")
-	# 	while self.hashtable[hashval][0] != key:
-	# 		i+=1
-	# 		hashval = self.hash_func(key) + i*self.probing_hash(key)
-	# 	return self.hashtable[hashval]
 
+	def __setitem__(self, key, value):
+		i=0
+		hashval = (self.hash_func(key) + i*self.probing_hash(key))%self.size 
+		while self.hashtable[hashval][0] != None and i < self.size:
+			i+=1
+			hashval = (self.hash_func(key) + i*self.probing_hash(key))%self.size 
+		if i < self.size:
+			self.hashtable[hashval][0] = key
+			self.hashtable[hashval][1] = value
+		else:
+			raise RuntimeError("Unable to set item")
 
-	# def __setattr__(self, key, value):
-	# 	i=0
-	# 	hashval = self.hash_func(key) + i*self.probing_hash(key)
-	# 	while self.hashtable[hashval][0] != None or self.hashtable[hashval][0] != "DEL":
-	# 		i+=1
-	# 		hashval = self.hash_func(key) + i*self.probing_hash(key)
-	# 	self.hashtable[hashval] = (key, value)	
+	def delete_item(self, key):
+		i=0
+		hashval = (self.hash_func(key) + i*self.probing_hash(key))%self.size 
+		while self.hashtable[hashval][0] != key and i < self.size:
+			i+=1
+			hashval = (self.hash_func(key) + i*self.probing_hash(key))%self.size 
+		if i < self.size:
+			self.hashtable[hashval][1] = None
+			self.hashtable[hashval][0] = "DEL"	
+		else:
+			raise RuntimeError("Unable to delete item")
+
 
 
 if __name__ == '__main__':
